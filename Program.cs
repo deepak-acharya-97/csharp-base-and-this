d47af0b1bc9da29faca3ebb8a6d1e0d3d52a1c3d
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-17 11:53:09 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace play_with_base_and_this
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
